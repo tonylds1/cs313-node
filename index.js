a6eb58ef22b9e0565925d007f35164c15ab40836
Tony Moraes <tony@voxtecnologia.com.br>
2019-07-17 19:11:34 -0300

    the first page

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 //Database Postgres
-const pool = require('./connection');
+// const pool = require('./connection');
 
 const personService = require('./services/personService')();
 const personRouter = require('./routes/personRouter')(express, personService);
 
 
 express()
+  .use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
   .use(express.static(path.join(__dirname, 'public')))
-  .use('/person', personRouter)
+  // .use('/person', personRouter)
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
