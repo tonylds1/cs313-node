26280f5d671ff0ac0d575f1fd114a45309723c12
Tony Moraes <tony@voxtecnologia.com.br>
2019-07-18 19:55:09 -0300

    [last modifications

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const $ = require('jquery');
 
 //Database Postgres
 // const pool = require('./connection');
@@ -11,6 +12,8 @@ const personRouter = require('./routes/personRouter')(express, personService);
 
 express()
   .use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+    .use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
+    .use('/js', express.static(__dirname + '/node_modules/jquery/dist/js'))
   .use(express.static(path.join(__dirname, 'public')))
   // .use('/person', personRouter)
   .set('views', path.join(__dirname, 'views'))
